b9fcd72670eee8ba4035ebaeb8d15d95388afac9
Daniel Torrico Buitrago <danieltorricob10@gmail.com>
2025-02-09 12:22:53 -0400

    Chapter3

---
+++ my_hostel/__manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Hostel Management',
+    'summary': 'Manage Hostel easily',
+    'description': "Efficiently manage the entire residential facility in the school.",
+    'author': 'Daniel',
+    'website': 'http://www.example.com',
+    'category': 'Uncategorized',
+    'version': '17.0.1.0.0',
+    'depends': ['base'],
+    'data': [
+        'security/hostel_security.xml',
+        'security/ir.model.access.csv',
+        'views/hostel.xml',
+    ],
+    #'assets': {
+    #    'web.assets_backend': [
+    #        'web/static/src/xml/**/*',
+    #    ],
+    #},
+    #'demo': ['demo.xml'],
+}
