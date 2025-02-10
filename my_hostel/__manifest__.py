d92c04a3c3d38924c11bcc66fba7f709c0664a97
Daniel Torrico Buitrago <danieltorricob10@gmail.com>
2025-02-09 23:19:35 -0400

    Chapter4

--- my_hostel/__manifest__.py
+++ my_hostel/__manifest__.py
@@ -10,7 +10,12 @@
     'data': [
         'security/hostel_security.xml',
         'security/ir.model.access.csv',
+        'data/data.xml',
         'views/hostel.xml',
+        'views/hostel_room.xml',
+        'views/hostel_amenities.xml',
+        'views/hostel_student.xml',
+        'views/hostel_categ.xml',
     ],
     #'assets': {
     #    'web.assets_backend': [
